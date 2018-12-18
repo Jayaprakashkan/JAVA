be83e72fc6c9425b1471ff7740e3705f243386dd
Jayaprakash <45662037+Jayaprakashkan@users.noreply.github.com>
2018-12-18 20:18:20 +0530

    Create Node.java

---
+++ Singly- Linked -List/Node.java
@@ -0,0 +1,11 @@
+/**
+ * Single Linked List
+ * 
+ * @author K.jayaprakash 
+ * 
+ */
+   //Creating Structure for Node
+public class Node {
+       int data;               //value
+       Node next;              //refer next object
+}
